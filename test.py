1b8792a80c08979b27ddcd5cc3488be35dff50d6
Gottipati Ajay Kumar <gottipati.ajaykumar@telusinternational.com>
2026-06-12 21:33:46 +0530

    feat: add cloudbuild config and gcs upload function

--- test.py
+++ test.py
@@ -2,16 +2,23 @@ import requests
 import pandas as pd
 
 API = "https://air-quality-api.open-meteo.com/v1/air-quality?latitude=17.3850&longitude=78.4867&hourly=pm2_5&timezone=Asia%2FKolkata&forecast_days=1"
+#GCP project
+PROJECT_ID = "bqdemo-496217"
+#GCP bucket name
+BUCKET_NAME = "bqdemo-496217-bucket"
+#GCP blob name
+LOCAL_CSV_FILE = "air_quality_data.csv" 
+
 
 response = requests.get(API).json()
 hourly_dict = response["hourly"]
 df = pd.DataFrame(hourly_dict)
+df.to_csv(LOCAL_CSV_FILE,index= False)
 
 
 
-#with open("output.txt", "w", encoding="utf-8") as file:
-    #file.write(df.to_string(index=False))
+
     
 
-#print("Text file written successfully!")
+print("Text file written successfully!")
 print(df)
